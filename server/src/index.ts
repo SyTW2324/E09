0ca5e7620cf17b0518243330b007c08a36698836
SergioPerera <alu0101394503@ull.edu.es>
2023-11-17 11:13:17 +0000

    asda

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,8 +2,10 @@ import { app } from './app.js';
 
 const port = process.env.PORT;
 
-app.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+// app.listen(port, () => {
+//   console.log(`Server is up on port ${port}`);
+// });
+
+app.listen(3000, () => {
+  console.log(`Server is up on port 3000`);
 });
-
-
