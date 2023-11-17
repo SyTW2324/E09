aa2db8882e1c6839ce544ea11f7a39b5bcb7451c
Aday <alu0101437538@ull.edu.es>
2023-11-17 11:07:37 +0000

    dev bdd

--- server/src/db/mongoose.ts
+++ server/src/db/mongoose.ts
@@ -1,7 +1,7 @@
 import { connect } from 'mongoose';
 
 try {
-  await connect(process.env.MONGODB_URL!);
+  await connect(process.env.ATLAS_URI!);
   console.log('Connection to MongoDB server established');
 } catch (error) {
   console.log(error);
