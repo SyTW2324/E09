c0e9e8802a86c7d81b0b159e8f7bfe2b6c846f1f
SergioPerera <alu0101394503@ull.edu.es>
2023-11-30 13:47:08 +0000

    Ahí te dejo un pequeño perdigón

---
+++ client/react-typescript-app/src/App.tsx
@@ -0,0 +1,80 @@
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import FooterBar from "./components/footer";
+// import NavScroll from "./components/header";
+// import HousingImage from './imgs/casas_blancas.png';
+// import Background from './imgs/background.jpg';
+
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <NavScroll />
+//       </header>
+//       <main>
+//         <div className="row mx-auto align-items-center" style={{ height: '100vh' }}>
+//           <div className="col-lg-6 text-end order-first">
+//             <div>
+//               {/* TODO: Reduce image width and text */}
+//               <h1 className="cheap-housing abyssinica-sil">CHEAP HOUSING</h1>
+//               <h2 className="cheap-housing abeezee-font">
+//                 This innovative company redefines hospitality by diverse and
+//                 unique accommodations, connecting travelers with authentic experiences.
+//               </h2>
+//             </div>
+//           </div>
+//           <div className="col-lg-6 text-center order-last">
+//             <img
+//               src={HousingImage}
+//               alt="casas_blancas"
+//               style={{ width: '100%', height: 'auto' }}
+//             />
+//           </div>
+//         </div>
+//         {/* <h1>Hello World</h1> */}
+//         <div className="tree-background">
+//           <h1>ANYAAAAAAAAAAAAAAAAAA</h1>
+//         {/* <img
+//               src={Background}
+//               alt="background"
+//               style={{ width: '100%', height: 'auto' }}
+//             /> */}
+//         </div>
+//       </main>
+//       <footer className='App-footer'>
+//         <FooterBar />
+//       </footer>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// Filename - App.js
+
+import React from "react";
+import Navbar from "./components/header";
+import {
+	BrowserRouter as Router,
+	Routes,
+	Route,
+} from "react-router-dom";
+import Home from "./pages";
+import Register from "./pages/register";
+import Login from "./pages/login";
+
+function App() {
+	return (
+		<Router>
+			{/* <Navbar /> */}
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/register" element={<Register />} />
+				<Route path="/login" element={<Login />} />
+			</Routes>
+		</Router>
+	);
+}
+
+export default App;
