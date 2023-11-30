fbcdeb95f3900f90436f762dd58e4b4a83570876
Aday <alu0101437538@ull.edu.es>
2023-11-30 20:07:26 +0000

    Fix backend

--- server/src/functions/verifytoken.ts
+++ server/src/functions/verifytoken.ts
@@ -6,11 +6,8 @@ dotenv.config();
 
 
 export const verifyToken = async(req: any, res: any, next: any) => {
-    const authHeader =req.headers.authorization.split(' ')[1];
     try {
-      if (!authHeader) {
-        throw new Error('Unauthorized');
-      }
+      const authHeader =req.headers.authorization.split(' ')[1];
       console.log(authHeader)
       const decodedUserInfo = jwt.verify(authHeader, process.env.AUTH_TOKEN_KEY!) as JwtPayload;
       const user = await User.find({ email: decodedUserInfo.email, password: decodedUserInfo.password });
@@ -20,7 +17,7 @@ export const verifyToken = async(req: any, res: any, next: any) => {
   
       req.user = {...user, ...decodedUserInfo};
     } catch (error) {
-      return res.status(403).send(error);
+      return res.status(403).send('Unauthorized');
     }
     return next();
   }
