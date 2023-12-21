740a5d84e9ce2833666bf3bd4066896ea1b1055d
SergioPerera <alu0101394503@ull.edu.es>
2023-12-21 19:33:28 +0000

    asda

--- client/cypress/support/component.ts
+++ client/cypress/support/component.ts
@@ -15,7 +15,7 @@
 
 // Import commands.js using ES2015 syntax:
 import './commands'
-
+import '@cypress/code-coverage/support';
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
 
@@ -37,3 +37,21 @@ Cypress.Commands.add('mount', mount)
 
 // Example use:
 // cy.mount(<MyComponent />)
+
+// // cypress.config.js
+// const { defineConfig } = require('cypress')
+
+// module.exports = defineConfig({
+//   // setupNodeEvents can be defined in either
+//   // the e2e or component configuration
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       require('@cypress/code-coverage/task')(on, config)
+//       // include any other plugin code...
+
+//       // It's IMPORTANT to return the config object
+//       // with any changed environment variables
+//       return config
+//     },
+//   },
+// })
