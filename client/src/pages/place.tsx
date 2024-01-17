cc596cf0c32beb5a310984bbecf09ff0922c976f
SergioPerera <alu0101394503@ull.edu.es>
2024-01-17 13:58:00 +0000

    carousel con img funcional

--- client/src/pages/place.tsx
+++ client/src/pages/place.tsx
@@ -99,8 +99,8 @@ export default function ProfilePlace() {
                       className="rounded-circle"
                       style={{ width: '150px' }}
                       fluid />
-                    <p className="text-muted mb-1">Full Stack Developer</p>
-                    <p className="text-muted mb-4">Bay Area, San Francisco, CA</p>
+                    <p className="text-muted mb-1">{user?.name}</p>
+                    <p className="text-muted mb-4">{user?.surname}</p>
                     <div className="d-flex justify-content-center mb-2">
                       <MDBBtn>Follow</MDBBtn>
                       <MDBBtn outline className="ms-1">Message</MDBBtn>
